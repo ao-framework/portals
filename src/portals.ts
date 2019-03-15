1f1f33a0b6c01dce27243eba44cffaae050d2cea
Josh Miller <wickedhillsmedia@gmail.com>
2019-03-14 20:39:18 -0400

    add comments to portals

--- src/portals.ts
+++ src/portals.ts
@@ -1,8 +1,19 @@
 import Portal from "./portal";
 
+/**
+ * Definition of the Portals
+ */
 export default class Portals {
+
+    /**
+     * Holds the table of portals
+     */
     private portals: { [key: string]: Portal } = {}
 
+    /**
+     * Retrieves and/or creates a portal
+     * @param name The name of the portal
+     */
     public portal(name: string) {
         if (!this.portals[name]) {
             this.portals[name] = new Portal(name);
@@ -10,6 +21,11 @@ export default class Portals {
         return this.portals[name];
     }
 
+    /**
+     * Broadcast to all portals
+     * @param event 
+     * @param args 
+     */
     public broadcast(event: string | symbol, ...args: any[]) {
         for (var iterator in this.portals) {
             this.portals[iterator].emit(event, ...args);
