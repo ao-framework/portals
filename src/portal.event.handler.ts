7500a3dd2307ca9e2e05e72cbef262462369140b
Josh Miller <wickedhillsmedia@gmail.com>
2019-03-14 21:01:56 -0400

    commented the event handler

--- src/portal.event.handler.ts
+++ src/portal.event.handler.ts
@@ -1,5 +1,21 @@
+/**
+ * Defintion of the portal event handler
+ */
 export default class PortalEventHandler {
+
+    /**
+     * The number of times that this listener
+     * can be called
+     */
     public count: number = null;
+
+    /**
+     * The context for this listener
+     */
     public context: any;
+
+    /**
+     * Holds the listener that will be fired
+     */
     public listener: (...args: any[]) => any;
 }
