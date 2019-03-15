5334f0f2478d60d8df3f25e26f438d69b385c252
Josh Miller <wickedhillsmedia@gmail.com>
2019-03-14 21:00:21 -0400

    worked on comments for the portal event

--- src/portal.event.ts
+++ src/portal.event.ts
@@ -1,8 +1,26 @@
 import PortalEventHandler from "./portal.event.handler";
 
+/**
+ * Definition of a portal event
+ */
 export default class PortalEvent {
+
+    /**
+     * The name or type of the channel
+     */
     public type: string | symbol;
+
+    /**
+     * The list of the handlers for the event
+     */
     public handlers: PortalEventHandler[] = [];
+
+    /**
+     * Helper method to stop extra handlers
+     * from being added when the max number
+     * has been met
+     * @param max 
+     */
     public getHandlersIfAllowed(max: number) {
         if (this.handlers.length >= max) {
             throw new Error(`Memory leak detected in portal (${this.type.toString()}). Max number of listeners exceeded.`);
